6a39dc1168c3e3ff911bccebee256a18f47c9d42
Antonio Molinari <molinari@incode.it>
2017-05-26 17:17:07 +0200

    index

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+  console.log(req.user)
+  res.render('index', {user: req.user });
 });
 
 app.get('/account', ensureAuthenticated, function(req, res){

45a024de4769b732c34be39851ff3e5adc95f11c
Beatrice <Beatrice@MacBook-Pro-di-Beatrice.local>
2017-05-26 16:31:57 +0200

    setup

--- configuration/config.js
+++ configuration/config.js
@@ -4,8 +4,8 @@ module.exports={
 * It consists of Twitter app information, database information.
 */
 
-	"facebook_api_key" 		: 			"",
-	"facebook_api_secret"	:				"",
+	"facebook_api_key" 		: 			"1685752108400876",
+	"facebook_api_secret"	:				"204b8d7435fd73520f3f947bda7a8b39",
 	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
 	"use_database"				:				"false",
 	"host"								:				"localhost",
